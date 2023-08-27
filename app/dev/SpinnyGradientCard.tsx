90ed5d2164027bf7882a8dcd549e8868aeeae015
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-08-27 15:21:26 +0800

    icon

--- app/dev/SpinnyGradientCard.tsx
+++ app/dev/SpinnyGradientCard.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 
                 <div className="border-[1px] border-gray-300 shadow-lg bg-white rounded-xl w-80 h-60 group flex flex-col items-center justify-center font-heading">
 
-                    Hello I'm a card
+                    Hello Im a card
 
                     <div
                         className="bg-gradient-to-r from-p-5 to-a1-5 opacity-50 hidden object-fill w-[500px] h-[500px] absolute rounded-full blur-md -z-10 group-hover:animate-spin group-hover:block"
--- app/dev/page copy.tsx
+++ app/dev/page copy.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 
                 <div className=" bg-white rounded-xl w-80 h-60 group flex flex-col items-center justify-center font-heading">
 
-                    Hello I'm a card
+                    Hello Im a card
 
                     <img src="/Ellipse 3.png" alt=""
                         className="hidden object-fill w-[500px] h-[500px] absolute left-0 blur-md z-10 group-hover:animate-spin group-hover:block"
